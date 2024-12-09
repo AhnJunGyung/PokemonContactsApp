d952576ea12490d82e9feafd8da137f7746c9c20
AhnJunGyung <ekdlrkzm@naver.com>
2024-12-09 14:49:55 +0900

    Initial Commit

---
+++ PokemonContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokemonContactsApp
+//
+//  Created by t2023-m0072 on 12/9/24.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("ViewDidLoad")
+    }
+
+
+}
