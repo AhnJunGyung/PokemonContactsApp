6c4c3c86d791aace70ac3dc652478b51c92608be
AJG <84224280+AhnJunGyung@users.noreply.github.com>
2024-12-15 18:41:19 +0900

    Delete PokemonContactsApp directory

--- PokemonContactsApp/CoreDataRepository.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CoreDataRepository.swift
-//  PokemonContactsApp
-//
-//  Created by t2023-m0072 on 12/12/24.
-//
-
-import Foundation
--- PokemonContactsApp/List/ListView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ListView.swift
-//  PokemonContactsApp
-//
-//  Created by t2023-m0072 on 12/12/24.
-//
-
-import Foundation
